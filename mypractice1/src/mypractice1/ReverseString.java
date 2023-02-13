5a36720317e21583cf26ea3d532afb2a38c32d8e
Ravi Maurya <ravimauryaa1@gmail.com>
2023-02-13 07:50:54 +0530

    added reverseString

---
+++ mypractice1/src/mypractice1/ReverseString.java
@@ -0,0 +1,28 @@
+package mypractice1;
+
+import java.util.Scanner;
+
+public class ReverseString {
+	
+	public void getreverse(String input) {
+		char ch[]=input.toCharArray();
+		String out="";
+		for(int i=ch.length-1;i>=0;i--) {
+			
+			out=out+ch[i];
+		}
+		System.out.println(out);
+	}
+
+	public static void main(String[] args) {
+		ReverseString ob = new ReverseString();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Please enter the input: ");
+		String input=sc.next();
+		ob.getreverse(input);
+			
+		
+
+	}
+
+}
