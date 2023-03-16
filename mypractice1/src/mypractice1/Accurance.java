fc8a7f0f78fc49e0d97465635921a6b8c810319b
Ravi Maurya <ravimauryaa1@gmail.com>
2023-03-16 11:17:05 +0530

    today1603

---
+++ mypractice1/src/mypractice1/Accurance.java
@@ -0,0 +1,20 @@
+package mypractice1;
+
+import java.util.Scanner;
+
+public class Accurance {
+	
+	public void getduplicate(int a[], int b[]) {
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+		Accurance ob=new Accurance();
+		Scanner sc=new Scanner(System.in);
+		String st="";
+		
+	}
+
+}
